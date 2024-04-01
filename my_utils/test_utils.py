1cd334271165ae58a4ace403c2fe6755b631bddf
hoo223 <lhs223@gmail.com>
2024-04-02 07:28:04 +0900

    update

--- my_utils/test_utils.py
+++ my_utils/test_utils.py
@@ -1560,3 +1560,47 @@ def get_pose_height(pose_2d):
         return pose_2d[:, :, 1].max(axis=-1) - pose_2d[:, :, 1].min(axis=-1)
     elif len(pose_2d.shape) == 4:
         return pose_2d[:, :, :, 1].max(axis=-1) - pose_2d[:, :, :, 1].min(axis=-1)
+    
+def procrustes_align(predicted, target):
+    """
+    Pose error: MPJPE after rigid alignment (scale, rotation, and translation),
+    often referred to as "Protocol #2" in many papers.
+    predicted: (T, 17, 3)
+    target: (T, 17, 3)
+    """
+    assert predicted.shape == target.shape
+    
+    # Translation
+    muX = np.mean(target, axis=1, keepdims=True)
+    muY = np.mean(predicted, axis=1, keepdims=True)
+    
+    X0 = target - muX
+    Y0 = predicted - muY
+
+    # Uniform scaling
+    normX = np.sqrt(np.sum(X0**2, axis=(1, 2), keepdims=True))
+    normY = np.sqrt(np.sum(Y0**2, axis=(1, 2), keepdims=True))
+    
+    X0 /= normX
+    Y0 /= normY
+
+    # Rotation
+    H = np.matmul(X0.transpose(0, 2, 1), Y0)
+    U, s, Vt = np.linalg.svd(H)
+    V = Vt.transpose(0, 2, 1)
+    R = np.matmul(V, U.transpose(0, 2, 1))
+
+    # Avoid improper rotations (reflections), i.e. rotations with det(R) = -1
+    sign_detR = np.sign(np.expand_dims(np.linalg.det(R), axis=1))
+    V[:, :, -1] *= sign_detR
+    s[:, -1] *= sign_detR.flatten()
+    R = np.matmul(V, U.transpose(0, 2, 1)) # Rotation
+    tr = np.expand_dims(np.sum(s, axis=1, keepdims=True), axis=2)
+    a = tr * normX / normY # Scale
+    t = muX - a*np.matmul(muY, R) # Translation
+    
+    # Perform rigid transformation on the input
+    predicted_aligned = a*np.matmul(predicted, R) + t
+    
+    # Return MPJPE
+    return predicted_aligned

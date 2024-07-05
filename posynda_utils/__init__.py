fccf4a8fad1cb10fb501f5f56f9ce14ad5b2dfa2
hoo223 <lhs223@gmail.com>
2024-07-05 21:13:00 +0900

    update

--- posynda_utils/__init__.py
+++ posynda_utils/__init__.py
@@ -1,10 +1,10 @@
 # PoSynDA library
 import sys, os
-sys.path.append('/home/hrai/codes/PoSynDA')
-from common.h36m_dataset import Human36mDataset
+sys.path.append('/home/hrai/codes/hpe_library')
+from posynda_utils.common.h36m_dataset import Human36mDataset
 lib_posynda = ['Human36mDataset']
 
 lib_except_my_utils = lib_posynda
-
-os.chdir('/home/hrai/codes/hpe_library')
+ 
+#os.chdir('/home/hrai/codes/hpe_library')
 __all__ = lib_except_my_utils
